950b189279fa401837414b42736614e7ad3d9b8a
Michael Taylor <patternizer@gmail.com>
2020-12-09 04:32:19 +0000

    Fixed note frequency mapping for LMMS to MIDI piano scale C(-1) to G9

--- climate-music.py
+++ climate-music.py
@@ -31,7 +31,8 @@ from miditime.miditime import MIDITime
 
 fontsize = 20
 load_crutem = True
-noctaves = 2 # either side of anomaly=0
+noctaves = 4 # either side of anomaly=0
+nkeys = 108
 
 #------------------------------------------------------------------------------
 # CHOOSE DATASET: 
@@ -92,31 +93,41 @@ df['yearly_mean'] = df[df.columns[range(1,13)]].mean(axis=1)
 data_array = np.array(df['yearly_mean'])
 
 # Set anomaly=0 as A440 and create 88 piano key map to +/- 2 octaves centered on 0 anomaly:
-musicmap_array = np.array(np.linspace(-noctaves, noctaves, 88))
+#musicmap_array = np.array(np.linspace(-noctaves, noctaves, nkeys))
+#musicmap_array = np.array(np.linspace(-4, (4+3/12), nkeys)) # 88-keys from A0
+musicmap_array = np.array(np.linspace(-(4+10/12), (4+2/12), nkeys)) # 108-keys from C0
 
-# Map data onto 88 array and align nearest bin values:
+# Map data onto 88-key or 108-key array and align nearest bin values:
 indices = nearest_indices(musicmap_array, data_array)
 mapped_array = [ musicmap_array[indices[i].astype(int)] for i in range(len(indices)) ]
+
+# Note frequency array (A440 --> n=49 on an 88-key piano from A0=27.5 Hz)
+# Note frequency array (A440 --> n=57 on an 108-key piano from C0=16.3516 Hz)
+
+#musicfreq_array = [ 440 * (2**(1/12))**((n+1)-49) for n in range(nkeys) ] # 88-keys from A0
+musicfreq_array = [ 440 * (2**(1/12))**((n+1)-58) for n in range(nkeys) ] # 108-keys from C0
+
+# Map data values onto frequencies
+notefreq_array = [ musicfreq_array[indices[i].astype(int)] for i in range(len(indices)) ]
 
 # Check status of data discretisation
 
 fig,ax = plt.subplots(figsize=(15,10))
 plt.step(df.year, data_array, color='red', label='raw data')
-plt.step(df.year, mapped_array, color='blue', label='88 bin alignment')
-#plt.step(df.year, mapped_array, color='blue', ls='dashed', label='88 bin alignment')
-plt.ylim(-noctaves,noctaves)
+plt.step(df.year, mapped_array, color='blue', label=str(nkeys)+' bin alignment')
+#plt.ylim(-noctaves,noctaves)
 plt.ylabel('Yearly anomaly', fontsize=fontsize)
 plt.title(datafile, fontsize=fontsize)
 ax.yaxis.grid(True, which='major')    
 ax.tick_params(axis='both', which='major', labelsize=fontsize)     
 plt.legend(loc='upper left', fontsize=12)
-plt.savefig('data_discretisation.png')
+plt.savefig('crutem4_data_discretisation.png')
 
 # Histogram of discretised values
 
 fig,ax = plt.subplots(figsize=(15,10))
-plt.hist(data_array, color='red', bins=88, label='raw data')    
-plt.hist(mapped_array, color='blue', bins=88, label='88 bin alignment') 
+plt.hist(data_array, color='red', bins=nkeys, label='raw data')    
+plt.hist(mapped_array, color='blue', bins=nkeys, label=str(nkeys)+' bin alignment') 
 plt.xlim(-noctaves,noctaves)
 plt.xlabel('Yearly anomaly', fontsize=fontsize)
 plt.ylabel('Count', fontsize=fontsize)
@@ -124,13 +135,7 @@ plt.title(datafile, fontsize=fontsize)
 ax.yaxis.grid(True, which='major')    
 ax.tick_params(axis='both', which='major', labelsize=fontsize)     
 plt.legend(loc='upper left', fontsize=12)
-plt.savefig('data_discretisation_histogram.png')
-
-# Note frequency array (A440 --> n=49)
-musicfreq_array = [ 440 * (2**(1/12))**((n+1)-49) for n in range(88) ] 
-
-# Map data values onto frequencies
-notefreq_array = [ musicfreq_array[indices[i].astype(int)] for i in range(len(indices)) ]
+plt.savefig('crutem4_data_discretisation_histogram.png')
 
 fig,ax = plt.subplots(figsize=(15,10))
 plt.step(df.year, notefreq_array, color='blue', label='values as notes')
@@ -144,7 +149,7 @@ plt.title(datafile, fontsize=fontsize)
 ax.yaxis.grid(True, which='major')   
 ax.tick_params(axis='both', which='major', labelsize=fontsize)     
 plt.legend(loc='upper left', fontsize=12)
-plt.savefig('data_note_frequencies-ylinear.png')
+plt.savefig('crutem4_data_note_frequencies-ylinear.png')
 
 fig,ax = plt.subplots(figsize=(15,10))
 plt.step(df.year, notefreq_array, color='blue', label='values as notes')
@@ -159,19 +164,20 @@ ax.yaxis.grid(True, which='major')
 ax.set_yscale('log') 
 ax.tick_params(axis='both', which='major', labelsize=fontsize)     
 plt.legend(loc='upper left', fontsize=12)
-plt.savefig('data_note_frequencies-ylog.png')
+plt.savefig('crutem4_data_note_frequencies-ylog.png')
 
 # Start MIDI class with tempo 120 beats per minute [bpm]:
     
 climate_midi = MIDITime(120, cru_music)
 
 # Create note array [beats, note, velocity, duration]
+# NB: MIDI scale is from C(-1) to G9 (8.175799 to 12543.85 Hz)
 
 duration = 1     # 1 beat per datum 
 velocity = 127   # 
 climate_song = []
 for i in range(len(data_array)):
-    climate_song.append([i, int(indices[i]), velocity, duration])
+    climate_song.append([i, int(indices[i]+12), velocity, duration]) # 108-keys from C0 --> +12 for MIDI
 
 # Add track and save .midi file
 
